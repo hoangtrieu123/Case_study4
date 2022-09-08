1915db24f46136cb73b0114719d9cdbdffbf8f4d
hoangtrieu123 <bimtrieu1998@gmail.com>
2022-09-08 08:23:15 +0700

    case

--- src/main/resources/client/js/post.js
+++ src/main/resources/client/js/post.js
@@ -1,3 +1,4 @@
+showAll()
 
 function createPost() {
     let title = $('#title').val()
@@ -28,12 +29,13 @@ function createPost() {
             Swal.fire({
                 position: 'center', icon: 'success', title: 'Thành công :D', showConfirmButton: false, timer: 1500
             })
-            document.getElementById("name").value = ""
-            document.getElementById("password").value = ""
-            document.getElementById("nameCustomer").value = ""
-            document.getElementById("telephone").value = ""
-            document.getElementById("email").value = ""
-            document.getElementById("jobProvince").value = ""
+            document.getElementById("title").value = ""
+            document.getElementById("salary").value = ""
+            document.getElementById("benefits").value = ""
+            document.getElementById("description").value = ""
+            document.getElementById("language").value = ""
+            document.getElementById("skill").value = ""
+            document.getElementById("address").value = ""
         }
     })
     event.preventDefault()
@@ -49,31 +51,78 @@ function showAll() {
     })
 }
 
-
-function displayPost(data){
-    let content=""
+function displayPost1(data) {
+    let content = ""
     for (let i = 0; i < data.length; i++) {
         content += "<div class='job pagi'>"
         content += "<div class='job-content'>"
         content += "<div class='job-logo'>"
         content += "<a href='#'>"
-        content += "<img src='../static/"+data[i].business.imageUrl+"' class='job-logo-ima' alt='job-logo'>"
+        content += "<img src='../static/" + data[i].business.imageUrl + "' class='job-logo-ima' alt='job-logo'>"
         content += "</a>"
         content += "</div>"
         content += "<div class='job-desc'>"
         content += "<div class='job-title'>"
-        content += "<a href='#'>"+data[i].title+"</a>"
+        content += "<a href='jd-page.html'>" + data[i].title + "</a>"
         content += "</div>"
         content += "<div class='job-company'>"
-        content += "<a href='#'>"+ data[i].business.nameBusiness +"</a> | <a href='#' class='job-address'><i class='fa fa-map-marker' aria-hidden='true'></i>"+data[i].address+"</a>"
+        content += "<a href='#'>" + data[i].business.nameBusiness + "</a> | <a href='#' class='job-address'><i class='fa fa-map-marker' aria-hidden='true'></i>" + data[i].address + "</a>"
         content += "</div>"
         content += "<div class='job-inf'>"
         content += "<div class='job-main-skill'>"
-        content += "<i class='fa fa-code' aria-hidden='true'></i><a href='#'>"+data[i].language+"</a>"
+        content += "<i class='fa fa-code' aria-hidden='true'></i><a href='#'>" + data[i].language + "</a>"
         content += "</div>"
         content += "<div id='salary' class='job-salary'>"
         content += "<i class='fa fa-money' aria-hidden='true'></i>"
-        content += "<span class='salary-min'>"+ data[i].salary +"<em class='salary-unit'> VND</em></span>"
+        content += "<span class='salary-min'>" + data[i].salary + "<em class='salary-unit'> VND</em></span>"
+        content += "</div>"
+        content += "</div>"
+        content += "</div>"
+        content += "<div class='wrap-btn-appl'>"
+        content += "<button onclick='edit(" + data[i].id + ")'>Chỉnh/Sửa bài đăng</button>"
+        content += "</div>"
+        content += "</div>"
+        content += "</div>"
+    }
+    document.getElementById("test1").innerHTML = content
+}
+
+showAll1()
+
+function showAll1() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/posts",
+        success: function (data) {
+            displayPost1(data);
+        }
+    })
+}
+
+function displayPost(data) {
+    let content = ""
+    for (let i = 0; i < data.length; i++) {
+        content += "<div class='job pagi'>"
+        content += "<div class='job-content'>"
+        content += "<div class='job-logo'>"
+        content += "<a href='#'>"
+        content += "<img src='../static/" + data[i].business.imageUrl + "' class='job-logo-ima' alt='job-logo'>"
+        content += "</a>"
+        content += "</div>"
+        content += "<div class='job-desc'>"
+        content += "<div class='job-title'>"
+        content += "<a href='jd-page.html'>" + data[i].title + "</a>"
+        content += "</div>"
+        content += "<div class='job-company'>"
+        content += "<a href='#'>" + data[i].business.nameBusiness + "</a> | <a href='#' class='job-address'><i class='fa fa-map-marker' aria-hidden='true'></i>" + data[i].address + "</a>"
+        content += "</div>"
+        content += "<div class='job-inf'>"
+        content += "<div class='job-main-skill'>"
+        content += "<i class='fa fa-code' aria-hidden='true'></i><a href='#'>" + data[i].language + "</a>"
+        content += "</div>"
+        content += "<div id='salary' class='job-salary'>"
+        content += "<i class='fa fa-money' aria-hidden='true'></i>"
+        content += "<span class='salary-min'>" + data[i].salary + "<em class='salary-unit'> VND</em></span>"
         content += "</div>"
         content += "</div>"
         content += "</div>"
@@ -83,7 +132,7 @@ function displayPost(data){
         content += "</div>"
         content += "</div>"
     }
-    document.getElementById("test").innerHTML=content
+    document.getElementById("test").innerHTML = content
 }
 
 
@@ -92,11 +141,65 @@ function search() {
     console.log(name)
     $.ajax({
         type: "GET",
-        url: "http://localhost:8080/posts/search/" + name ,
-        success: function (data){
+        url: "http://localhost:8080/posts/search/" + name,
+        success: function (data) {
             console.log(data)
             displayPost(data)
         }
     })
     event.preventDefault()
+}
+
+
+let idJD
+
+function edit(id) {
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/posts/"+id,
+        success: function (data) {
+            idJD = data.id
+            document.getElementById("title").value = data.title
+            document.getElementById("salary").value = data.salary
+            document.getElementById("benefits").value = data.benefits
+            document.getElementById("description").value = data.description
+            document.getElementById("language").value = data.language
+            document.getElementById("skill").value = data.skill
+            document.getElementById("address").value = data.address
+            document.getElementById("button").setAttribute("onclick", "editPost()")
+        }
+
+    })
+}
+
+function editPost() {
+    let title = $('#title').val()
+    let salary = $('#salary').val()
+    let benefits = $('#benefits').val()
+    let description = $('#description').val()
+    let language = $('#language').val()
+    let skill = $('#skill').val()
+    let address = $('#address').val()
+    let post = {
+        id: idJD,
+        title: title,
+        salary: salary,
+        benefits: benefits,
+        description: description,
+        language: language,
+        skill: skill,
+        address: address,
+    }
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "PUT",
+        url: "/http://localhost:8080/posts",
+        data: JSON.stringify(post),
+    })
+    event.preventDefault()
+
 }
